bc48373a4cb5d5fce3d0815443f086693a53aefa
Oliver Bell <freshollie@gmail.com>
2016-12-15 03:05:14 +0000

    More additions

--- app/src/main/java/com/freshollie/headunitcontroller/NLService.java
+++ app/src/main/java/com/freshollie/headunitcontroller/NLService.java
@@ -6,6 +6,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.support.v4.app.NotificationCompat;
@@ -14,68 +15,43 @@ import android.util.Log;
 public class NLService extends NotificationListenerService {
 
     private String TAG = this.getClass().getSimpleName();
-    private NLServiceReceiver nlServiceReceiver;
+    private SharedPreferences sharedPreferences;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        sharedPreferences =
+                getSharedPreferences(getString(R.string.PREFERENCES_KEY), Context.MODE_PRIVATE);
         Log.v(TAG, "Running");
-        nlServiceReceiver = new NLServiceReceiver();
-        IntentFilter filter = new IntentFilter();
-        filter.addAction("com.kpbird.nlsexample.NOTIFICATION_LISTENER_SERVICE_EXAMPLE");
-        registerReceiver(nlServiceReceiver,filter);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        unregisterReceiver(nlServiceReceiver);
     }
 
     @Override
     public void onNotificationPosted(StatusBarNotification notification) {
-
-        Log.i(TAG,"**********  onNotificationPosted");
-        Log.i(TAG,"ID :" + notification.getId() + "\t" + notification.getNotification().tickerText + "\t" + notification.getPackageName());
-        Intent i = new  Intent("com.kpbird.nlsexample.NOTIFICATION_LISTENER_EXAMPLE");
-        i.putExtra("notification_event","onNotificationPosted :" + notification.getPackageName() + "\n");
-        sendBroadcast(i);
+        Log.v(TAG, "Posted " + notification.getPackageName());
+        if (notification.getPackageName() == "com.google.android.apps.maps" &&
+                !sharedPreferences.getBoolean(getString(R.string.DRIVING_MODE_KEY), false)
+                ) {
+            Log.v(TAG, "Driving mode enabled");
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean(getString(R.string.DRIVING_MODE_KEY), true);
+            editor.apply();
+        }
 
     }
 
     @Override
-    public void onNotificationRemoved(StatusBarNotification sbn) {
-        Log.i(TAG,"********** onNotificationRemoved");
-        Log.i(TAG,"ID :" + sbn.getId() + "\t" + sbn.getNotification().tickerText +"\t" + sbn.getPackageName());
-        Intent i = new  Intent("com.kpbird.nlsexample.NOTIFICATION_LISTENER_EXAMPLE");
-        i.putExtra("notification_event","onNotificationRemoved :" + sbn.getPackageName() + "\n");
-
-        sendBroadcast(i);
-    }
-
-    class NLServiceReceiver extends BroadcastReceiver{
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if(intent.getStringExtra("command").equals("clearall")){
-                NLService.this.cancelAllNotifications();
-            }
-            else if(intent.getStringExtra("command").equals("list")){
-                Intent i1 = new  Intent("com.kpbird.nlsexample.NOTIFICATION_LISTENER_EXAMPLE");
-                i1.putExtra("notification_event","=====================");
-                sendBroadcast(i1);
-                int i=1;
-                for (StatusBarNotification sbn : NLService.this.getActiveNotifications()) {
-                    Intent i2 = new  Intent("com.kpbird.nlsexample.NOTIFICATION_LISTENER_EXAMPLE");
-                    i2.putExtra("notification_event",i +" " + sbn.getPackageName() + "\n");
-                    sendBroadcast(i2);
-                    i++;
-                }
-                Intent i3 = new  Intent("com.kpbird.nlsexample.NOTIFICATION_LISTENER_EXAMPLE");
-                i3.putExtra("notification_event","===== Notification List ====");
-                sendBroadcast(i3);
-
-            }
-
+    public void onNotificationRemoved(StatusBarNotification notification) {
+        if (PowerUtil.isConnected(getApplicationContext()) &&
+                notification.getPackageName() == "com.google.android.apps.maps") {
+            Log.v(TAG, "Driving mode disabled");
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean(getString(R.string.DRIVING_MODE_KEY), false);
+            editor.apply();
         }
     }
 
--- app/src/main/java/com/freshollie/headunitcontroller/RoutineService.java
+++ app/src/main/java/com/freshollie/headunitcontroller/RoutineService.java
@@ -1,12 +1,28 @@
 package com.freshollie.headunitcontroller;
 
+import android.app.ActivityManager;
 import android.app.Service;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.hardware.usb.UsbManager;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.session.MediaController;
+import android.media.session.MediaSessionManager;
+import android.net.Uri;
+import android.os.Handler;
 import android.os.IBinder;
 import android.os.SystemClock;
 import android.util.Log;
+import android.view.KeyEvent;
+
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 /**
  * Created by Freshollie on 14/12/2016.
@@ -17,48 +33,129 @@ public class RoutineService extends Service {
     public static int ALL_DEVICES = 0;
     public static int ATTACH_TIMEOUT = 3000; // Milliseconds
 
+    public static String CONTEXT_USAGE_STATS_MANAGER = "usagestats";
+
+    private SharedPreferences sharedPreferences;
+    private MediaPlayer mediaPlayer;
+
     public interface OnAllDevicesAttachedListener{
         void onAllAttached();
     }
 
     @Override
     public void onCreate() {
+        sharedPreferences = getSharedPreferences(
+                getString(R.string.PLAYING_AUDIO_APP_KEY),
+                Context.MODE_PRIVATE
+        );
 
     }
 
-    public void waitForAttached(final OnAllDevicesAttachedListener listener){
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                UsbManager usbManager =
-                        (UsbManager) getApplicationContext().getSystemService(Context.USB_SERVICE);
+    public void playBlankAudio() {
+        stopBlankAudio();
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.blank);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
+        Log.v(TAG, "Blank audio started");
+    }
 
-                long startTime = SystemClock.currentThreadTimeMillis();
+    public void stopBlankAudio(){
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+            Log.v(TAG, "Blank audio stopped");
+        }
+    }
 
-                while (usbManager.getDeviceList().size() <ALL_DEVICES){
-                    if ((SystemClock.currentThreadTimeMillis() - startTime) > ATTACH_TIMEOUT) {
-                        // Wait timed out running routing anyway
-                        break;
-                    }
-                }
+    public void raiseVolume() {
+        Log.v(TAG, "Raising system volume");
+        ((AudioManager) getSystemService(Context.AUDIO_SERVICE))
+                .setStreamVolume(AudioManager.STREAM_MUSIC, 13, 0);
+    }
 
-                listener.onAllAttached();
-            }
-        }).start();
+    public void playAudioApp(String packageName) {
+        Log.v(TAG, "Playing " + packageName);
+        long eventTime = SystemClock.uptimeMillis();
+
+        Intent downIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
+        KeyEvent downEvent = new KeyEvent(eventTime, eventTime,
+                KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE, 0);
+        downIntent.putExtra(Intent.EXTRA_KEY_EVENT, downEvent);
+        downIntent.setPackage(packageName);
+        sendOrderedBroadcast(downIntent, null);
+
+        Intent upIntent = new Intent(Intent.ACTION_MEDIA_BUTTON, null);
+        KeyEvent upEvent = new KeyEvent(eventTime, eventTime,
+                KeyEvent.ACTION_UP, KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE, 0);
+        upIntent.putExtra(Intent.EXTRA_KEY_EVENT, upEvent);
+        upIntent.setPackage(packageName);
+        sendOrderedBroadcast(upIntent, null);
+    }
+
+    public void playLastAudioSource() {
+        String lastPlayingAudioApp =
+                sharedPreferences.getString(getString(R.string.PLAYING_AUDIO_APP_KEY), null);
+
+        if (lastPlayingAudioApp != null) {
+            playAudioApp(lastPlayingAudioApp);
+        }
+    }
+
+    public void launchMaps() {
+        Intent intent = new Intent(Intent.ACTION_VIEW)
+                .setData(Uri.parse("google.navigation:/?free=1&mode=d&entry=fnls"))
+                .setPackage("come.google.android.apps.maps");
+        startActivity(intent);
+    }
+
+    public void checkMapsForeground() {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        if (getForegroundPackageName().equals("com.google.android.apps.maps")) {
+            editor.putBoolean(getString(R.string.LAUNCH_MAPS_KEY), true);
+        } else {
+            editor.putBoolean(getString(R.string.LAUNCH_MAPS_KEY), false);
+        }
+        editor.apply();
     }
 
     public void runStartRoutine() {
+        Log.v(TAG, "Running start routine");
+        playLastAudioSource();
+        //raiseVolume();
 
+        if (sharedPreferences.getBoolean(getString(R.string.LAUNCH_MAPS_KEY), false) &&
+                sharedPreferences.getBoolean(getString(R.string.DRIVING_MODE_KEY), false)) {
+            launchMaps();
+        }
     }
 
     public void runStopSequence() {
-
+        Log.v(TAG, "Running stop routine");
+        stopBlankAudio();
+        checkMapsForeground();
     }
 
     @Override
     public int onStartCommand(final Intent intent, final int flags, final int startId) {
-        Log.v(TAG, "Routine starting");
         Log.v(TAG, intent.getAction());
+
+        switch (intent.getAction()) {
+            case Intent.ACTION_POWER_CONNECTED:
+                Handler handler = new Handler(getMainLooper());
+                int delay = 1000; //milliseconds
+
+                handler.postDelayed(new Runnable() {
+                    public void run() {
+                        runStartRoutine();
+                    }
+                }, delay);
+                return START_NOT_STICKY;
+
+            case Intent.ACTION_POWER_DISCONNECTED:
+                runStopSequence();
+                return START_NOT_STICKY;
+        }
         return START_NOT_STICKY;
     }
 
@@ -72,5 +169,34 @@ public class RoutineService extends Service {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
     }
+
+    public String getForegroundPackageName(){
+        String currentApp = "";
+
+        //noinspection ResourceType
+        UsageStatsManager usm = (UsageStatsManager) getSystemService(CONTEXT_USAGE_STATS_MANAGER);
+
+        long time = System.currentTimeMillis();
+        List<UsageStats> appList = usm.queryUsageStats(UsageStatsManager.INTERVAL_DAILY,
+                time - 1000 * 1000, time);
+        // Gets the apps used in the last second
+
+        if (appList != null && appList.size() > 0) {
+            SortedMap<Long, UsageStats> mySortedMap = new TreeMap<>();
+
+            for (UsageStats usageStats : appList) {
+                mySortedMap.put(usageStats.getLastTimeUsed(),
+                        usageStats);
+            }
+
+            if (!mySortedMap.isEmpty()) {
+                // Get the top app
+                currentApp = mySortedMap.get(
+                        mySortedMap.lastKey()).getPackageName();
+            }
+        }
+
+        return currentApp;
+    };
 
 }
