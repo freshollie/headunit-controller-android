235c259e5c06a73ac2466f8d8ab0d42be2f7f52f
Oliver Bell <freshollie@gmail.com>
2017-05-03 02:11:42 +0100

    Updated keymapping API, continued to impliment key mapping UI

--- app/src/main/java/com/freshollie/headunitcontroller/ui/KeySetDialog.java
+++ app/src/main/java/com/freshollie/headunitcontroller/ui/KeySetDialog.java
@@ -1,19 +1,29 @@
 package com.freshollie.headunitcontroller.ui;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.pm.PackageInfo;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
 
 import com.freshollie.headunitcontroller.R;
 import com.freshollie.headunitcontroller.input.DeviceInputManager;
 import com.freshollie.headunitcontroller.input.DeviceKeyMapper;
 import com.freshollie.shuttlexpressdriver.ShuttleXpressDevice;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by freshollie on 28/04/17.
@@ -21,37 +31,93 @@ import com.freshollie.shuttlexpressdriver.ShuttleXpressDevice;
 
 public class KeySetDialog extends DialogFragment {
 
+    private String TAG = this.getClass().getSimpleName();
+
     private DeviceKeyMapper keyMapper;
     private int key = ShuttleXpressDevice.KeyCodes.BUTTON_0;
+
+    private DeviceKeyMapper.ActionMap pressAction;
+    private DeviceKeyMapper.ActionMap holdAction;
+    private int holdDelay;
+
+    private Spinner pressActionSpinner;
+    private EditText pressExtraEditText;
+
+    private EditText holdDelayEditText;
+    private Spinner holdActionSpinner;
+    private EditText holdExtraEditText;
 
     public void setKey(int key) {
         this.key = key;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.keyset_dialog_fragment, container, false);
-
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
         keyMapper = new DeviceKeyMapper(getActivity());
+
+        AlertDialog.Builder b = new AlertDialog.Builder(getActivity())
+                .setTitle(DeviceInputManager.getNameForDeviceKey(getActivity(), key))
+                .setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                // do something...
+                            }
+                        }
+                )
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                dialog.dismiss();
+                            }
+                        }
+                );
+
+        LayoutInflater i = getActivity().getLayoutInflater();
+
+
+        pressAction = keyMapper.getKeyPressAction(key);
+        holdAction = keyMapper.getKeyHoldAction(key);
+        holdDelay = keyMapper.getKeyHoldDelay(key);
+
+
+        @SuppressLint("InflateParams")
+        View v = i.inflate(R.layout.keyset_dialog_fragment, null);
 
         refreshDetails(v);
 
-        return v;
+        b.setView(v);
+        return b.create();
+    }
+
+    public HashMap<String, String> getInstalledPackages() {
+        List<PackageInfo> packageInfos = getActivity().getPackageManager().getInstalledPackages(0);
+        HashMap<String, String> packageNames = new HashMap<>();
+
+        for (PackageInfo info: packageInfos) {
+            packageNames.put(
+                    info.packageName,
+                    info.applicationInfo.loadLabel(getActivity().getPackageManager()).toString()
+            );
+        }
+
+        return packageNames;
     }
 
     public void refreshDetails(View v) {
 
-        ((TextView) v.findViewById(R.id.key_title))
-                .setText(DeviceInputManager.getNameForDeviceKey(getActivity(), key));
+        pressActionSpinner = (Spinner) v.findViewById(R.id.press_action_spinner);
+        pressExtraEditText = (EditText) v.findViewById(R.id.press_extra_input);
 
-        Spinner pressActionSpinner = (Spinner) v.findViewById(R.id.press_action_spinner);
+        holdActionSpinner = (Spinner) v.findViewById(R.id.hold_action_spinner);
+        holdDelayEditText = (EditText) v.findViewById(R.id.hold_delay_input);
+        holdExtraEditText = (EditText) v.findViewById(R.id.hold_extra_input);
 
-        String[] actionStrings = new String[DeviceInputManager.ALL_ACTIONS.length];
+
+        String[] actionStrings = new String[DeviceInputManager.ACTIONS.length];
 
         int i = 0;
 
-        for (String action: DeviceInputManager.ALL_ACTIONS) {
+        for (String action: DeviceInputManager.ACTIONS) {
             actionStrings[i] = DeviceInputManager.getStringForAction(getActivity(), action);
             i++;
         }
@@ -59,12 +125,15 @@ public class KeySetDialog extends DialogFragment {
         pressActionSpinner.setAdapter(
                 new ArrayAdapter<>(getActivity(), android.R.layout.simple_dropdown_item_1line,
                         actionStrings));
+        pressActionSpinner.setSelection(pressAction.getActionId());
 
         pressActionSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                keyMapper.setKeyAction(key, DeviceInputManager.ALL_ACTIONS[i]);
-                refreshDetails(getView());
+                if (pressAction.getActionId() != i) {
+                    pressAction = new DeviceKeyMapper.ActionMap(i, null);
+                    pressExtraEditText.setText("");
+                }
             }
 
             @Override
@@ -72,5 +141,106 @@ public class KeySetDialog extends DialogFragment {
 
             }
         });
+
+        pressExtraEditText.setText(pressAction.getReadableExtra(getActivity()));
+
+        pressExtraEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        holdDelayEditText.setText(String.valueOf(keyMapper.getKeyHoldDelay(key)));
+        holdDelayEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        holdActionSpinner.setAdapter(
+                new ArrayAdapter<>(getActivity(), android.R.layout.simple_dropdown_item_1line,
+                        actionStrings));
+        holdActionSpinner.setSelection(holdAction.getActionId());
+
+        holdActionSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (holdAction.getActionId() != i) {
+                    holdAction = new DeviceKeyMapper.ActionMap(i, null);
+                    holdExtraEditText.setText("");
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        holdExtraEditText.setText(holdAction.getReadableExtra(getActivity()));
+        holdExtraEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (holdAction.getAction().equals(DeviceInputManager.ACTION_LAUNCH_APP)) {
+                    showSelectAppDialog(holdExtraEditText, holdAction);
+                }
+            }
+        });
+
+    }
+
+    public void showSelectAppDialog(final EditText resultHolder, final DeviceKeyMapper.ActionMap editMap) {
+        final HashMap<String, String> packages = getInstalledPackages();
+
+        final String[] packageNames = packages.keySet()
+                .toArray(new String[packages.keySet().toArray().length]);
+
+        String[] appNames = packages.values()
+                .toArray(new String[packages.values().size()]);
+
+        Arrays.sort(appNames, new Comparator<String>() {
+            @Override
+            public int compare(String s, String t1) {
+                return s.compareTo(t1);
+            }
+        });
+
+        Arrays.sort(packageNames, new Comparator<String>() {
+            @Override
+            public int compare(String s, String t1) {
+                return packages.get(s).compareTo(packages.get(t1));
+            }
+        });
+
+        int packageNum = -1;
+
+        for (int i = 0; i < packageNames.length; i++) {
+            if (packageNames[i].equals(holdAction.getExtra())) {
+                packageNum = i;
+                break;
+            }
+        }
+
+        new AlertDialog.Builder(getActivity())
+                .setTitle("Select Application")
+                .setSingleChoiceItems(appNames, packageNum,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                editMap.setExtra(packageNames[i]);
+
+                                resultHolder.setText(holdAction.getReadableExtra(getActivity()));
+                                dialogInterface.dismiss();
+                            }
+                        })
+                .setPositiveButton(android.R.string.cancel,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.dismiss();
+                            }
+                        }).show();
     }
 }
